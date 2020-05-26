bcbe8a27b9ba7b804bf96e6b15e89ec316546c7f
Glenny De Cock <glenny.de.cock@codifly.be>
2020-05-26 20:14:33 +0200

    chore: add joi foundation.

--- src/schema/capsule/capsuleResolvers.ts
+++ src/schema/capsule/capsuleResolvers.ts
@@ -1,9 +1,20 @@
+import joi from '@hapi/joi';
+
 import { Capsule } from '../../contracts';
 import { capsuleFacade } from '../../facades';
 
 const capsuleResolvers = {
   Query: {
     capsules: async (_: any, args: any): Promise<Capsule[]> => {
+      const schema = joi.object({
+        limit: joi.number().optional(),
+        offset: joi.number().max(3).optional(),
+        order: joi.number().optional(),
+      });
+
+      const result = schema.validate(args);
+      console.log(result);
+
       return capsuleFacade.capsules(args);
     },
     capsule: async (_: any, args: any): Promise<Capsule> => {
