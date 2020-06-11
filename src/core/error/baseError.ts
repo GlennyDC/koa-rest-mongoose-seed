05d668c7c4352bb54b5b7822e63763c46abd7857
Glenny De Cock <glenny.de.cock@codifly.be>
2020-06-11 19:36:15 +0200

    chore: make error fields readonly.

--- src/core/error/baseError.ts
+++ src/core/error/baseError.ts
@@ -1,7 +1,7 @@
 abstract class BaseError extends Error {
-  code: string;
-  httpStatusCode: number;
-  timestamp: string;
+  readonly code: string;
+  readonly httpStatusCode: number;
+  readonly timestamp: string;
 
   constructor(message: string, code: string, httpStatusCode: number) {
     super(message);
@@ -10,10 +10,10 @@ abstract class BaseError extends Error {
     this.timestamp = new Date().toISOString();
 
     /**
-     * Make the name property of the error non-enumerable.
-     * Which means that it won't show up when iterated through the object
-     * using Object.keys() or for...in loop. You can still access the property
-     * directly on the object though.
+     * Make the name property of the error non-enumerable, which means that
+     * it won't show up when iterated through the object using Object.keys()
+     * or a for...in loop. You can still access the property directly
+     * on the object though.
      */
     Object.defineProperty(this, 'name', { value: this.constructor.name });
   }
--- src/core/error/inputValidationError.ts
+++ src/core/error/inputValidationError.ts
@@ -2,7 +2,7 @@ import { BaseError } from './baseError';
 import { ErrorCode } from './errorCode';
 
 class InputValidationError extends BaseError {
-  validationErrors: Record<string, string>;
+  readonly validationErrors: Record<string, string>;
 
   constructor(validationErrors: Record<string, string>) {
     super('Input validation failed', ErrorCode.INPUT_INVALID, 400);
