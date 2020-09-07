89c58502365af603eafa8c389bd82560494db4e2
Glenny De Cock <glenny.de.cock@codifly.be>
2020-09-07 22:04:35 +0200

    Add proper error handling on startup

--- src/app.ts
+++ src/app.ts
@@ -15,19 +15,34 @@ const createApp = async (): Promise<Koa> => {
   const app = new Koa();
 
   logger.info('Applying middleware...');
-  applyMiddleware(app);
+  try {
+    applyMiddleware(app);
+  } catch (err) {
+    logger.error('Could not apply middleware:', err);
+    throw err;
+  }
 
   logger.info('Installing Apollo server...');
-  installApolloServer(app);
+  try {
+    installApolloServer(app);
+  } catch (err) {
+    logger.error('Could not install Apollo server:', err);
+    throw err;
+  }
 
   logger.info('Installing database connection...');
-  await installDatabaseConnection();
+  try {
+    await installDatabaseConnection();
+  } catch (err) {
+    logger.error('Could not install database connection:', err);
+    throw err;
+  }
 
   // Log any handled Koa error
   // (will probably never occur except for 404 Not found)
   app.on('error', (err) => {
     logger.info(
-      'Following error was correctly handled in error middleware: ',
+      'Following error was correctly handled in error middleware:',
       err,
     );
   });
--- src/index.ts
+++ src/index.ts
@@ -10,47 +10,56 @@ const HOST_NAME = config.server.hostName;
 const logger = createLogger('server');
 
 (async (): Promise<void> => {
-  const app = await createApp();
+  try {
+    const app = await createApp();
 
-  const server = http
-    .createServer(app.callback())
-    .listen(PORT, HOST_NAME, () => {
-      logger.info(
-        `Server ready at ${HOST_NAME}:${PORT} in ${process.env.NODE_ENV} mode`,
-      );
+    const server = http
+      .createServer(app.callback())
+      .listen(PORT, HOST_NAME, () => {
+        logger.info(
+          `Server ready at ${HOST_NAME}:${PORT} in ${process.env.NODE_ENV} mode`,
+        );
+      });
+
+    // Graceful shutdown of the server
+    const shutdown = (): void => {
+      logger.info('Starting shutdown of server...');
+      server.close((err) => {
+        if (err) {
+          logger.error(`Could not gracefully close server: `, err);
+          process.exitCode = 1;
+        }
+        process.exit();
+      });
+    };
+
+    // SIGINT signal (CTRL-C)
+    process.on('SIGINT', () => {
+      logger.warn('Received SIGINT signal');
+      shutdown();
     });
 
-  // Graceful shutdown of the server
-  const shutdown = (): void => {
-    logger.info('Starting shutdown of server...');
-    server.close((err) => {
-      if (err) {
-        logger.error(`Could not gracefully close server: `, err);
-        process.exitCode = 1;
-      }
-      process.exit();
+    // SIGTERM signal (Docker stop)
+    process.on('SIGTERM', () => {
+      logger.warn('Received SIGTERM signal');
+      shutdown();
     });
-  };
 
-  // SIGINT signal (CTRL-C)
-  process.on('SIGINT', () => {
-    logger.warn('Received SIGINT signal');
-    shutdown();
-  });
+    process.on('uncaughtException', (error) => {
+      logger.error('Uncaught exception: ', error);
+      shutdown();
+    });
 
-  // SIGTERM signal (Docker stop)
-  process.on('SIGTERM', () => {
-    logger.warn('Received SIGTERM signal');
-    shutdown();
-  });
-
-  process.on('uncaughtException', (error) => {
-    logger.error('Uncaught exception: ', error);
-    shutdown();
-  });
-
-  process.on('unhandledRejection', (reason, promise) => {
-    logger.error('Unhandled rejection: ', JSON.stringify(reason), promise);
-    shutdown();
-  });
+    process.on('unhandledRejection', (reason, promise) => {
+      logger.error('Unhandled rejection: ', JSON.stringify(reason), promise);
+      shutdown();
+    });
+  } catch (err) {
+    // TODO: Delete this
+    logger.error(
+      'Wooah there, this should never ever occur ########### (except during a startup error):',
+      err,
+    );
+    process.exit(1);
+  }
 })();
