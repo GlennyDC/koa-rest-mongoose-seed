e60f9a95accf9b48d96e682242a0903129f9a56c
Glenny De Cock <glenny.de.cock@codifly.be>
2020-10-27 21:55:47 +0100

    Add jest testing skeleton

---
+++ test/modules/organisation.test.ts
@@ -0,0 +1,13 @@
+describe('Organisation module', () => {
+  describe('query > organisation', () => {
+    test.todo('should fetch a single organisation');
+  });
+
+  describe('query > organisations', () => {
+    test.todo('should fetch a list of organisations');
+  });
+
+  describe('mutation > createOrganisation', () => {
+    test.todo('should create an organisation');
+  });
+});
---
+++ test/modules/user.test.ts
@@ -0,0 +1,35 @@
+describe('User module', () => {
+  describe('mutation > register', () => {
+    test.todo('should throw on user with a number as email address');
+
+    test.todo('should throw on user with an empty string as email address');
+
+    test.todo('should throw on user with a malformed email address');
+
+    test.todo('should throw on user with a number as password');
+
+    test.todo('should throw on user with an empty string as password');
+
+    test.todo('should throw on user with an email address that already exists');
+
+    test.todo('should register a user with a valid email address and password');
+  });
+
+  describe('mutation > login', () => {
+    test.todo('should throw on user with a number as email address');
+
+    test.todo('should throw on user with an empty string as email address');
+
+    test.todo('should throw on user with a malformed email address');
+
+    test.todo('should throw on user with a number as password');
+
+    test.todo('should throw on user with an empty string as password');
+
+    test.todo('should throw on user with an email address that does not exist');
+
+    test.todo('should throw on user with invalid credentials');
+
+    test.todo('should login user with valid credentials');
+  });
+});
